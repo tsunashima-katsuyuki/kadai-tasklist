73ae470f71052d3dddbb5726b5d3d95835010779
tsunashima.katsuyuki <tsunashima.katsuyuki@e-jan.co.jp>
2019-04-22 15:51:38 +0900

    add validation

--- src/controllers/CreateServlet.java
+++ src/controllers/CreateServlet.java
@@ -2,8 +2,10 @@ package controllers;
 
 import java.io.IOException;
 import java.sql.Timestamp;
+import java.util.List;
 
 import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -11,8 +13,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import models.Task;
+import models.validators.TaskValidator;
 import utils.DBUtil;
-
 /**
  * Servlet implementation class CreateServlet
  */
@@ -45,13 +47,26 @@ public class CreateServlet extends HttpServlet {
             t.setCreated_at(currentTime);
             t.setUpdated_at(currentTime);
 
-            em.getTransaction().begin();
-            em.persist(t);
-            em.getTransaction().commit();
-            request.getSession().setAttribute("flush", "新規タスクの登録を行いました。");
-            em.close();
+            List<String> errors = TaskValidator.validate(t);
 
-            response.sendRedirect(request.getContextPath() + "/index");
+            if(errors.size()>0){
+                em.close();
+
+                request.setAttribute("_token", request.getSession().getId());
+                request.setAttribute("task", t);
+                request.setAttribute("errors", errors);
+                RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/tasks/new.jsp");
+                rd.forward(request, response);
+            }else{
+                em.getTransaction().begin();
+                em.persist(t);
+                em.getTransaction().commit();
+                request.getSession().setAttribute("flush", "新規タスクの登録を行いました。");
+                em.close();
+
+                response.sendRedirect(request.getContextPath() + "/index");
+            }
+
         }
     }
 
